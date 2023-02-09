1acc70163fce2575b2b1f8cbfa8a6ecba5a5ae75
yugo <yugo.hoshino1906@gmail.com>
2023-02-10 07:43:13 +0900

    add:push_swap.h

---
+++ push_swap.h
@@ -0,0 +1,21 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   push_swap.h                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: yughoshi <yughoshi@student.42tokyo.jp>     +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/02/08 22:24:08 by yughoshi          #+#    #+#             */
+/*   Updated: 2023/02/10 07:31:03 by yughoshi         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef PUSH_SWAP_H
+# define PUSH_SWAP_H
+
+# include "./include_libft/libft.h"
+# include "./include_printf/ft_printf.h"
+# include <stdlib.h>
+# include <limits.h>
+
+#endif
