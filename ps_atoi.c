d4ce90c3af2e974e4c8df1a3b3a76d3fcff29519
yugo <yugo.hoshino1906@gmail.com>
2023-02-12 22:53:04 +0900

    add:push_swap用のatoi関数の完成

---
+++ ps_atoi.c
@@ -0,0 +1,55 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ps_atoi.c                                          :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: yughoshi <yughoshi@student.42tokyo.jp>     +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/02/10 09:39:48 by yughoshi          #+#    #+#             */
+/*   Updated: 2023/02/12 22:51:27 by yughoshi         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "push_swap.h"
+
+static int	ft_isspace(int c)
+{
+	if (c == ' ' || c == '\f' || c == '\n')
+		return (1);
+	if (c == '\r' || c == '\t' || c == '\v')
+		return (1);
+	return (0);
+}
+
+int	ps_atoi(const char *str)
+{
+	long long	res;
+	int			sign;
+	size_t		i;
+
+	res = 0;
+	sign = 1;
+	i = 0;
+	while (ft_isspace(str[i]))
+		i++;
+	if (str[i] == '-' || str[i] == '+')
+	{
+		if (str[i] == '-')
+			sign = -1;
+		i++;
+	}
+	while (ft_isdigit(str[i]))
+	{
+		if (ft_isdigit(str[i]) == 0)
+			return (0);
+		if (sign == 1 && (res > ((LONG_MAX - (sign * (str[i] - '0'))) / 10)))
+			return ((int)LONG_MAX);
+		if (sign == -1 && (res < ((LONG_MIN - (sign * (str[i] - '0'))) / 10)))
+			return ((int)LONG_MIN);
+		res = (res * 10) + (sign * (str[i] - '0'));
+		i++;
+	}
+	if (!ft_isdigit(str[i]) && str[i] != '\0')
+		put_error_and_exit();
+	return ((int)res);
+}
