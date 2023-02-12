6fffb4d28698514947460a4dd7de5a99602b6cb0
yugo <yugo.hoshino1906@gmail.com>
2023-02-12 22:54:47 +0900

    update:構造体の宣言, atoi, put_error関数の宣言

--- push_swap.h
+++ push_swap.h
@@ -6,16 +6,28 @@
 /*   By: yughoshi <yughoshi@student.42tokyo.jp>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/02/08 22:24:08 by yughoshi          #+#    #+#             */
-/*   Updated: 2023/02/10 07:31:03 by yughoshi         ###   ########.fr       */
+/*   Updated: 2023/02/12 22:41:05 by yughoshi         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef PUSH_SWAP_H
 # define PUSH_SWAP_H
+# define FD_2 2
 
 # include "./include_libft/libft.h"
 # include "./include_printf/ft_printf.h"
-# include <stdlib.h>
 # include <limits.h>
+# include <stdlib.h>
+# include <unistd.h>
+
+typedef struct s_node
+{
+	struct s_node	*prev;
+	int				st_num;
+	struct s_node	*next;
+}					t_node;
+
+int					ps_atoi(const char *str);
+void				put_error_and_exit(void);
 
 #endif
