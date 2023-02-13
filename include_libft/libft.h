568ed6ffbafa6fb699c505bc50de6d12594719d3
yugo <yugo.hoshino1906@gmail.com>
2023-02-13 09:49:55 +0900

    fix:プロトタイプ宣言のエラー修正

--- include_libft/libft.h
+++ include_libft/libft.h
@@ -6,7 +6,7 @@
 /*   By: yughoshi <yughoshi@student.42tokyo.jp>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/10/06 09:14:58 by yughoshi          #+#    #+#             */
-/*   Updated: 2023/02/13 09:41:11 by yughoshi         ###   ########.fr       */
+/*   Updated: 2023/02/13 09:49:30 by yughoshi         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -63,7 +63,7 @@ t_list				*ft_lstnew(void *content);
 void				ft_lstadd_front(t_list **lst, t_list *new);
 int					ft_lstsize(t_list *lst);
 t_list				*ft_lstlast(t_list *lst);
-void				ft_lstadd_back(t_node *lst, t_node new);
+void				ft_lstadd_back(t_list **lst, t_list *new);
 void				ft_lstdelone(t_list *lst, void (*del)(void *));
 void				ft_lstclear(t_list **lst, void (*del)(void *));
 void				ft_lstiter(t_list *lst, void (*f)(void *));
