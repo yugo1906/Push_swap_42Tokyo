5bf4acf0ea6e428f8cea9e0579f6c8f6f8aecae7
yugo <yugo.hoshino1906@gmail.com>
2023-02-14 09:47:26 +0900

    fix:printfのヘッダーファイルを修正

--- include_printf/ft_printf.h
+++ include_printf/ft_printf.h
@@ -6,7 +6,7 @@
 /*   By: yughoshi <yughoshi@student.42tokyo.jp>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/10/20 16:17:56 by yughoshi          #+#    #+#             */
-/*   Updated: 2022/10/25 00:55:11 by yughoshi         ###   ########.fr       */
+/*   Updated: 2023/02/14 09:10:08 by yughoshi         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,7 +16,7 @@
 # include <stdarg.h>
 # include <unistd.h>
 
-int		ft_printf(const char *fmt, ...) __attribute__((format(printf, 1, 2)));
+int		ft_printf(const char *fmt, ...);
 void	my_putnbr_and_increment(long long n, int fd, int *res_len);
 void	my_putstr_and_increment(char *s, int fd, int *res_len);
 void	put_address(unsigned long long n, char *base_16, int *res_len);
