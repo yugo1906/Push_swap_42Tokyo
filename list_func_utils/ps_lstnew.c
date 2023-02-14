1baf56aa4ee838f0de924738cf4462a233ab1937
yugo <yugo.hoshino1906@gmail.com>
2023-02-15 08:30:59 +0900

    add:push_swap用のlstnew関数を追加

---
+++ list_func_utils/ps_lstnew.c
@@ -0,0 +1,29 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ps_lstnew.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: yughoshi <yughoshi@student.42tokyo.jp>     +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/02/13 21:56:13 by yughoshi          #+#    #+#             */
+/*   Updated: 2023/02/14 07:41:20 by yughoshi         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "../push_swap.h"
+
+t_node	*ps_lstnew(int content)
+{
+	t_node	*new_node;
+
+	new_node = (t_node *)malloc(1 * sizeof(t_node));
+	if (new_node == NULL)
+	{
+		//todo::これまでに確保したnodeすべてをfree:error_all_free()
+		return (NULL);
+	}
+	new_node->num = content;
+	new_node->next = NULL;
+	new_node->prev = NULL;
+	return (new_node);
+}
