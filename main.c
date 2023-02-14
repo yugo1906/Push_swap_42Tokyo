14da2806eb9087ae71f037a34b0e4382023549d3
yugo <yugo.hoshino1906@gmail.com>
2023-02-14 09:45:44 +0900

    update:stackとheadの初期化関数を実装

--- main.c
+++ main.c
@@ -6,27 +6,28 @@
 /*   By: yughoshi <yughoshi@student.42tokyo.jp>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/02/08 22:17:25 by yughoshi          #+#    #+#             */
-/*   Updated: 2023/02/14 09:23:00 by yughoshi         ###   ########.fr       */
+/*   Updated: 2023/02/14 09:44:59 by yughoshi         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "push_swap.h"
 #include <stdio.h>
 
-t_node	*init_stack(void)
+void	init_stack(t_node **head, t_node **stack)
 {
-	t_node	*new_node;
+	t_node	*new;
 
-	new_node = (t_node *)malloc(1 * sizeof(t_node));
-	if (new_node == NULL)
+	new = (t_node *)malloc(1 * sizeof(t_node));
+	if (new == NULL)
 	{
-		//todo::これまでに確保したnodeすべてをfree:error_all_free()
-		return (NULL);
+		// todo::これまでに確保したnodeすべてをfree:error_all_free_exit()
+		//  all_free_and_exit();
 	}
-	new_node->num = 0;
-	new_node->next = NULL;
-	new_node->prev = NULL;
-	return (new_node);
+	new->num = 0;
+	new->next = NULL;
+	new->prev = NULL;
+	*head = new;
+	*stack = *head;
 }
 
 int	main(int argc, char *argv[])
@@ -38,20 +39,21 @@ int	main(int argc, char *argv[])
 	t_node	*head_a;
 	t_node	*head_b;
 
-	head_a = init_stack();
-	head_b = init_stack();
-	stack_a = head_a;
-	stack_b = head_b;
+	init_stack(&head_a, &stack_a);
+	init_stack(&head_b, &stack_b);
+
 	if (argc == 1)
 		put_error_and_exit();
 	tmp_argc = argc - 1;
 	i = 0;
 	while (i++ < tmp_argc)
 	{
+		// ft_printf("i = %d\n", i);
 		stack_a->num = ps_atoi(argv[i]);
+		// ft_printf("%d\n", stack_a->num);
 		if (i != tmp_argc)
 		{
-			stack_a->next = init_stack();
+			stack_a->next = ps_lstnew(0);
 			stack_a = stack_a->next;
 		}
 	}
