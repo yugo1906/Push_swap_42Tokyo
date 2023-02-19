17a7f804eb5918c38ae305240b4944a982834923
yugo <yugo.hoshino1906@gmail.com>
2023-02-19 13:00:01 +0900

    update:rbコマンドの完成

--- main.c
+++ main.c
@@ -6,7 +6,7 @@
 /*   By: yughoshi <yughoshi@student.42tokyo.jp>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/02/08 22:17:25 by yughoshi          #+#    #+#             */
-/*   Updated: 2023/02/19 12:50:23 by yughoshi         ###   ########.fr       */
+/*   Updated: 2023/02/19 12:59:27 by yughoshi         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -56,7 +56,6 @@ int	main(int argc, char *argv[])
 			stack_a = stack_a->next;
 		}
 	}
-
 	/*todo::最後に消す スタック内を確認するテストコード */
 	//テストコード:stack_aの値確認
 	stack_a = head_a;
@@ -66,9 +65,12 @@ int	main(int argc, char *argv[])
 		ft_printf("stack_a->num = %d\n", stack_a->num);
 		stack_a = stack_a->next;
 	}
-	// raの挙動確認
+	// rotateの挙動確認
 	ft_printf("raの挙動確認===\n");
 	ra(&stack_a, &head_a);
+	ra(&stack_a, &head_a);
+	ra(&stack_a, &head_a);
+	rb(&stack_b, &head_b);
 	stack_a = head_a;
 	while (stack_a != NULL)
 	{
@@ -77,121 +79,43 @@ int	main(int argc, char *argv[])
 		i--;
 	}
 	// swapの挙動確認
-// 	sa(&stack_a, &head_a);
-// 	ft_printf("saの挙動確認===\n");
-// 	while (stack_a != NULL)
-// 	{
-// 		ft_printf("stack_a->num = %d\n", stack_a->num);
-// 		stack_a = stack_a->next;
-// 	}
-// 	pa(&head_a, &head_b);
-// 	pa(&head_a, &head_b);
-// 	pa(&head_a, &head_b);
-// 	pa(&head_a, &head_b);
-// 	// pa(&head_a, &head_b);
-// 	sb(&stack_b, &head_b);
-// 	ft_printf("ss前===\n");
-// 	stack_a = head_a;
-// 	stack_b = head_b;
-// 	while (stack_a != NULL)
-// 	{
-// 		ft_printf("stack_a->num = %d\n", stack_a->num);
-// 		stack_a = stack_a->next;
-// 	}
-// 	while (stack_b != NULL)
-// 	{
-// 		ft_printf("stack_b->num = %d\n", stack_b->num);
-// 		stack_b = stack_b->next;
-// 	}
-// 	stack_a = head_a;
-// 	stack_b = head_b;
-// 	ss(&stack_a, &head_a, &stack_b, &head_b);
-// 	while (stack_a != NULL)
-// 	{
-// 		ft_printf("stack_a->num = %d\n", stack_a->num);
-// 		stack_a = stack_a->next;
-// 	}
-// 	while (stack_b != NULL)
-// 	{
-// 		ft_printf("stack_b->num = %d\n", stack_b->num);
-// 		stack_b = stack_b->next;
-// 	}
-// 	/*
-// 	// sa(&stack_a, &head_a);
-// 	stack_a = head_a;
-// 	stack_b = head_b;
-// 	pa(&head_a, &head_b);
-// 	pa(&head_a, &head_b);
-// 	ft_printf("\n===pa後のstack_a===\n\n");
-// 	// pa(&stack_a, &head_a, &stack_b, &head_b);
-// 	stack_a = head_a;
-// 	while (stack_a != NULL)
-// 	{
-// 		ft_printf("stack_a->num = %d\n", stack_a->num);
-// 		stack_a = stack_a->next;
-// 	}
-// 	ft_printf("\n===pb前のstack_bの確認===\n\n");
-// 	stack_b = head_b;
-// 	while (stack_b != NULL)
-// 	{
-// 		ft_printf("stack_b->num = %d\n", stack_b->num);
-// 		stack_b = stack_b->next;
-// 	}
-// 	pb(&head_b, &head_a);
-// 	ft_printf("\n===pb後のstack_aの確認===\n\n");
-// 	stack_a = head_a;
-// 	while (stack_a != NULL)
-// 	{
-// 		ft_printf("stack_a->num = %d\n", stack_a->num);
-// 		stack_a = stack_a->next;
-// 	}
-// 	ft_printf("\n===最終のstack_bの確認===\n");
-// 	// テストコード:stack_bの値確認
-// 	stack_b = head_b;
-// 	while (stack_b != NULL)
-// 	{
-// 		ft_printf("stack_b->num = %d\n", stack_b->num);
-// 		stack_b = stack_b->next;
-// 	}
-// 	return (0);
-// }
-
-// int	main(int argc, char *argv[])
-// {
-// 	size_t	tmp_argc;
-// 	size_t	i;
-// 	t_node	*stack_a;
-// 	t_node	*stack_b;
-// 	t_node	*head_a;
-// 	t_node	*head_b;
-
-// 	head_a = init_stack();
-// 	head_b = init_stack();
-// 	stack_a = head_a;
-// 	stack_b = head_b;
-// 	if (argc == 1)
-// 		put_error_and_exit();
-// 	ft_printf("argc = %d\n", argc);
-// 	tmp_argc = argc - 1;
-// 	i = 0;
-// 	while (i++ < tmp_argc)
-// 	{
-// 		ft_printf("i = %d\n", i);
-// 		stack_a->num = ps_atoi(argv[i]);
-// 		ft_printf("%d\n", stack_a->num);
-// 		if (i != tmp_argc)
-// 		{
-// 			stack_a->next = init_stack();
-// 			stack_a = stack_a->next;
-// 		}
-// 	}
-// 	stack_a = head_a;
-// 	while (stack_a != NULL)
-// 	{
-// 		ft_printf("OK\n");
-// 		ft_printf("stack_a->num = %d\n", stack_a->num);
-// 		stack_a = stack_a->next;
-// 	}
-// */
+	sa(&stack_a, &head_a);
+	ft_printf("saの挙動確認===\n");
+	while (stack_a != NULL)
+	{
+		ft_printf("stack_a->num = %d\n", stack_a->num);
+		stack_a = stack_a->next;
+	}
+	pa(&head_a, &head_b);
+	pa(&head_a, &head_b);
+	// pa(&head_a, &head_b);
+	// sb(&stack_b, &head_b);
+	ft_printf("ss前===\n");
+	stack_a = head_a;
+	stack_b = head_b;
+	while (stack_a != NULL)
+	{
+		ft_printf("stack_a->num = %d\n", stack_a->num);
+		stack_a = stack_a->next;
+	}
+	while (stack_b != NULL)
+	{
+		ft_printf("stack_b->num = %d\n", stack_b->num);
+		stack_b = stack_b->next;
+	}
+	stack_a = head_a;
+	stack_b = head_b;
+	ft_printf("ss後===\n");
+	ss(&stack_a, &head_a, &stack_b, &head_b);
+	while (stack_a != NULL)
+	{
+		ft_printf("stack_a->num = %d\n", stack_a->num);
+		stack_a = stack_a->next;
+	}
+	while (stack_b != NULL)
+	{
+		ft_printf("stack_b->num = %d\n", stack_b->num);
+		stack_b = stack_b->next;
+	}
 	return (0);
 }
--- push_swap_cmd/rotate.c
+++ push_swap_cmd/rotate.c
@@ -6,7 +6,7 @@
 /*   By: yughoshi <yughoshi@student.42tokyo.jp>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/02/19 10:23:45 by yughoshi          #+#    #+#             */
-/*   Updated: 2023/02/19 12:51:06 by yughoshi         ###   ########.fr       */
+/*   Updated: 2023/02/19 12:53:03 by yughoshi         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,9 +17,6 @@ void	ra(t_node **stack, t_node **head)
 	size_t	i;
 
 	*stack = *head;
-	// if (!((*stack) == NULL) && !((*stack)->next == NULL))
-	// 	return ;
-	// リストの最後のノードを特定する処理
 	if (!((*stack) == NULL) && !((*stack)->next == NULL))
 	{
 		i = 0;
@@ -28,12 +25,9 @@ void	ra(t_node **stack, t_node **head)
 			*stack = (*stack)->next;
 			i++;
 		}
-		// リストの最後のノードがリストの先頭のnodeを指す処理
 		(*stack)->next = *head;
-		// リストの先頭のheadがリストの２番目を指し、先頭 == 2番目として扱う処理
 		*stack = *head;
 		*head = (*stack)->next;
-		// (元々先頭だったnodeが最後のノードの場所に移っており、)元々先頭だったnodeのnode->nextがNULLになる処理
 		*stack = *head;
 		while (!(i <= 0))
 		{
@@ -42,5 +36,32 @@ void	ra(t_node **stack, t_node **head)
 		}
 		(*stack)->next = NULL;
 		write(1, "ra\n", 3);
+	}
+}
+
+void	rb(t_node **stack, t_node **head)
+{
+	size_t i;
+
+	*stack = *head;
+	if (!((*stack) == NULL) && !((*stack)->next == NULL))
+	{
+		i = 0;
+		while ((*stack)->next)
+		{
+			*stack = (*stack)->next;
+			i++;
+		}
+		(*stack)->next = *head;
+		*stack = *head;
+		*head = (*stack)->next;
+		*stack = *head;
+		while (!(i <= 0))
+		{
+			*stack = (*stack)->next;
+			i--;
+		}
+		(*stack)->next = NULL;
+		write(1, "rb\n", 3);
 	}
 }
