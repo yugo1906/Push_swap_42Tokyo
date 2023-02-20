3c1a92083281b0f6d91b7c5df507009f91aa7de0
yugo <yugo.hoshino1906@gmail.com>
2023-02-21 08:53:34 +0900

    update:rraコマンドの完成_コメントあり

--- push_swap_cmd/reverse_rotate.c
+++ push_swap_cmd/reverse_rotate.c
@@ -6,8 +6,48 @@
 /*   By: yughoshi <yughoshi@student.42tokyo.jp>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/02/19 10:23:42 by yughoshi          #+#    #+#             */
-/*   Updated: 2023/02/19 10:24:02 by yughoshi         ###   ########.fr       */
+/*   Updated: 2023/02/21 08:52:29 by yughoshi         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../push_swap.h"
+
+// void	reverse_rotate(void)
+// {
+// }
+
+void	rra(t_node **stack, t_node **head)
+{
+	size_t	i;
+
+	i = 0;
+	*stack = *head;
+	if (*stack && (*stack)->next)
+	{
+		// 最後のnodeを特定する
+		while ((*stack)->next)
+		{
+			*stack = (*stack)->next;
+			i++;
+		}
+		// 最後のnodeが1番目のnodeを指す
+		(*stack)->next = *head;
+		*head = *stack;
+		while (i > 0)
+		{
+			*stack = (*stack)->next;
+			i--;
+		}
+		(*stack)->next = NULL;
+		*stack = *head;
+		write(1, "rra\n", 4);
+	}
+}
+
+// void	rrb(void)
+// {
+// }
+
+// void	rrr(void)
+// {
+// }
